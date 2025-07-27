c1bd3fd67b36e01021e6eaab424bcfa5c394156e
joehannaC <jhnnacansino@gmail.com>
2025-07-27 23:11:10 +0800

    asm for macos
    same code, made some adjustments for macos

--- MP2/compute_acceleration.asm
+++ MP2/compute_acceleration.asm
@@ -1,42 +1,41 @@
 section .data
     align 8
-    convert_const: dq 0.27777777778         ; double constant: 1000/3600
+    convert_const: dq 0.27777777778         ; double constant: 1000 / 3600
 
 section .text
-    global compute_acceleration
+    global _compute_acceleration            ; added "_" for macos
 
-compute_acceleration:
+_compute_acceleration:                      ; added "_" for macos
     push rbp
     mov rbp, rsp
     sub rsp, 16
     movsd [rsp], xmm7
+
     movsd xmm7, [rel convert_const]
 
-    xor r9d, r9d
+    xor ecx, ecx
 
 .loop:
-    cmp r9d, r8d
+    cmp ecx, edx        ; used edx for macos
     jge .done
 
-    mov eax, r9d
+    mov eax, ecx
     imul eax, 24
     mov rax, rax
 
-    movsd xmm0, [rcx + rax]         ; vi
-    movsd xmm1, [rcx + rax + 8]     ; vf
-    movsd xmm2, [rcx + rax + 16]    ; t
+    ; used rdi instead of rcx for macos
+    movsd xmm0, [rdi + rax]       ; vi
+    movsd xmm1, [rdi + rax + 8]   ; vf
+    movsd xmm2, [rdi + rax + 16]  ; t
 
-    subsd xmm1, xmm0        ; xmm1 = vf - vi
-    mulsd xmm1, xmm7        ; convert to m/s
-    divsd xmm1, xmm2        ; divide by time
+    subsd xmm1, xmm0              ; vf - vi
+    mulsd xmm1, xmm7              ; convert to m/s
+    divsd xmm1, xmm2              ; acceleration
 
-    ; Round and convert to int
-    cvtsd2si eax, xmm1                   ; eax = (int)acceleration
+    cvtsd2si eax, xmm1            ; convert to int
+    mov [rsi + rcx*4], eax        ; for results, used rsi instead of rdx for macos
 
-    ; Store to results
-    mov [rdx + r9*4], eax
-
-    inc r9d
+    inc ecx
     jmp .loop
 
 .done:
